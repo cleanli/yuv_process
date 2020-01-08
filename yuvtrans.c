e10090f56f55313cfedafef122fb51368845e88f
Clean Li <clean_li@yahoo.com>
2020-01-08 23:08:19 +0800

    add part of paras

--- yuvtrans.c
+++ yuvtrans.c
@@ -8,11 +8,27 @@
 #define UVSIZE (IMG_W * IMG_H / 2)
 
 
-int main()
+int main(int argc, char *argv[])
 {
        size_t ret = 0;
-       unsigned char c;
+       unsigned char thr=THRD;
        int width = IMG_W, height = IMG_H;
+       printf("argc %d\n", argc);
+       if(argc <= 6){
+               printf("Usage: yuv_process W H THR inputfile outputfile\n");
+       }
+       if(argc >= 2){
+               width = atoi(argv[1]);
+	       printf("W = %d\n", width);
+       }
+       if(argc >= 3){
+               height = atoi(argv[2]);
+	       printf("H = %d\n", height);
+       }
+       if(argc >= 4){
+               thr = atoi(argv[3]);
+	       printf("THR = %d\n", thr);
+       }
        unsigned char* input_line = (char*) malloc(2*width);
        if(!input_line){
                printf("can't malloc memory!\n");
@@ -35,8 +51,8 @@ int main()
                }
 	       for(int j=0;j<width;j++){
 	               //printf("i put j %x i[j] %x\n", j, input_line[j]);
-                       if(input_line[j] > THRD)output_line[j]=255;
-                       if(input_line[j] <=THRD)output_line[j]=0;
+                       if(input_line[j] > thr)output_line[j]=255;
+                       if(input_line[j] <=thr)output_line[j]=0;
 	       }
                fwrite(output_line, width, 1, fpo);
        }
