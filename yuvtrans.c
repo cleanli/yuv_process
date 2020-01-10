0df92412d546ec6fc7ff0fc33a6c0e05166cad99
Clean Li <clean_li@yahoo.com>
2020-01-10 22:17:38 +0800

    add print option

--- yuvtrans.c
+++ yuvtrans.c
@@ -8,17 +8,20 @@
 #define UVSIZE (IMG_W * IMG_H / 2)
 #define MAX_BYTES_FILENAME 128
 
+unsigned char abdiff(unsigned char x, unsigned char y);
 int main(int argc, char *argv[])
 {
        size_t ret = 0;
        unsigned char thr=THRD;
        int width = IMG_W, height = IMG_H;
+       int algo_flag = 0;
        char* inputfilename = "C:\\files\\ffmpeg\\xcb.yuv";
        char outputfilename[MAX_BYTES_FILENAME];
+       char* filename_end = "_out.yuv";
        memset(outputfilename, 0, MAX_BYTES_FILENAME);
        //printf("argc %d\n", argc);
-       if(argc <= 5){
-               printf("yuvprocess v0.1\nUsage: yuv_process W H inputfile THR(128)\n");
+       if(argc <= 6){
+               printf("yuvprocess v0.1\nUsage: yuv_process W H inputfile THR(128) print\n");
        }
        if(argc >= 2){
                width = atoi(argv[1]);
@@ -36,19 +39,29 @@ int main(int argc, char *argv[])
                thr = atoi(argv[4]);
 	       printf("THR = %d\n", thr);
        }
-       //printf("last is %s\n", inputfilename+strlen(inputfilename)-4);
-       sprintf(outputfilename, "%s_out.yuv", inputfilename);
-       if(strlen(inputfilename)>4 && !strcmp(".yuv", inputfilename+strlen(inputfilename)-4)){
-               sprintf(outputfilename+strlen(inputfilename)-4, "%s", "_out.yuv");
+       if(argc >= 6){
+               if(!strcmp("print",argv[5])){
+	           algo_flag = 1;
+                   filename_end = "_outP.yuv";
+	       }
+	       printf("print = %d\n", algo_flag);
        }
-       unsigned char* input_line = (char*) malloc(2*width);
+       //printf("last is %s\n", inputfilename+strlen(inputfilename)-4);
+       sprintf(outputfilename, "%s%s", inputfilename, filename_end);
+       if(strlen(inputfilename)>4 && !strcmp(".yuv", inputfilename+strlen(inputfilename)-4)){
+               sprintf(outputfilename+strlen(inputfilename)-4, "%s", filename_end);
+       }
+       unsigned char* input_line = (char*) malloc(3*width);
        if(!input_line){
                printf("can't malloc memory!\n");
 	       return -1;
        }
        unsigned char* output_line = input_line + width;
+       unsigned char* pre_input_line = input_line + width*2;
+       memset(input_line, 0, width);
        printf("start process...\n");
-       printf("yuv_process %d %d %s %d %s\n", width, height, inputfilename, thr, outputfilename);
+       printf("yuv_process %d %d %s %d %s print=%d\n",
+           width, height, inputfilename, thr, outputfilename, algo_flag);
        FILE *fpi = fopen(inputfilename, "rb");
        FILE *fpo = fopen(outputfilename, "wb");
        if(fpi == NULL || fpo == NULL){
@@ -57,15 +70,25 @@ int main(int argc, char *argv[])
        }
        for(unsigned int i=0;i<height;i++)
        {
+               memcpy(pre_input_line, input_line, width);
                ret = fread(input_line, width, 1, fpi);
                if(ret != 1){
                        printf("fread ret %d i %x err %s\n", ret, i, strerror(errno));
                        return -1;
                }
 	       for(int j=0;j<width;j++){
-	               //printf("i put j %x i[j] %x\n", j, input_line[j]);
+	           if(!algo_flag){
                        if(input_line[j] > thr)output_line[j]=255;
                        if(input_line[j] <=thr)output_line[j]=0;
+		   }
+		   else{
+                       unsigned char ddt;
+		       if(j == width -1) ddt = 0;
+		       else ddt = abdiff(input_line[j+1],input_line[j]);
+                       if(ddt > thr)output_line[j]=0;
+		       else if(abdiff(input_line[j], pre_input_line[j]) > thr) output_line[j] = 0;
+		       else output_line[j]=255;
+		   }
 	       }
                fwrite(output_line, width, 1, fpo);
        }
@@ -82,3 +105,8 @@ int main(int argc, char *argv[])
        fclose(fpo);
 }
 
+unsigned char abdiff(unsigned char x, unsigned char y)
+{
+        if(x > y)return x-y;
+	else return y-x;
+}
